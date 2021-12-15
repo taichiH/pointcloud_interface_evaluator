2565d4b39b5a6049b199e3f8f38652c4b64919bf
taichiH <azumade.30@gmail.com>
2021-12-15 19:47:20 +0900

    update

--- src/test.cpp
+++ src/test.cpp
@@ -22,15 +22,34 @@ struct Point {
                          const custom_type::Point &p2) noexcept {
     return true;
   }
-};
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+
+struct PointXYZI {
+  PCL_ADD_POINT4D;
+  float intensity;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+
 } // namespace custom_type
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(
+    custom_type::PointXYZI,
+    (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity))
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(
     custom_type::Point,
-    (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(
-        uint16_t, ring, ring)(float, azimuth, azimuth)(
-        float, distance, distance)(uint8_t, return_type,
-                                   return_type)(double, time_stamp, time_stamp))
+    (float, x,
+     x)(float, y,
+        y)(float, z,
+           z)(float, intensity,
+              intensity)(uint16_t, ring,
+                         ring)(float, azimuth,
+                               azimuth)(float, distance,
+                                        distance)(uint8_t, return_type,
+                                                  return_type)(double,
+                                                               time_stamp,
+                                                               time_stamp))
 
 LIDAR_UTILS__DEFINE_FIELD_GENERATOR_FOR_MEMBER(azimuth);
 LIDAR_UTILS__DEFINE_FIELD_GENERATOR_FOR_MEMBER(distance);
@@ -38,27 +57,29 @@ LIDAR_UTILS__DEFINE_FIELD_GENERATOR_FOR_MEMBER(return_type);
 LIDAR_UTILS__DEFINE_FIELD_GENERATOR_FOR_MEMBER(time_stamp);
 
 bool deserialize(std::vector<custom_type::Point> &pts,
-                 sensor_msgs::msg::PointCloud2 &msg) {
+                 const sensor_msgs::msg::PointCloud2::SharedPtr &msg) {
 
-  if (msg.data.size() != msg.row_step) {
+  if (msg->data.size() != msg->row_step) {
     printf("invariant data\n");
     return false;
   }
-  for (int idx = 0; idx < msg.row_step; idx += msg.point_step) {
-    unsigned char *addr = &msg.data[idx];
-    float x = *reinterpret_cast<float *>(addr + msg.fields.at(0).offset);
-    float y = *reinterpret_cast<float *>(addr + msg.fields.at(1).offset);
-    float z = *reinterpret_cast<float *>(addr + msg.fields.at(2).offset);
+  for (int idx = 0; idx < msg->row_step; idx += msg->point_step) {
+    // offsetの作られ方が統一されるなら構造体のタイプにキャストしてしまっても良い
+    unsigned char *addr = &msg->data[idx];
+    float x = *reinterpret_cast<float *>(addr + msg->fields.at(0).offset);
+    float y = *reinterpret_cast<float *>(addr + msg->fields.at(1).offset);
+    float z = *reinterpret_cast<float *>(addr + msg->fields.at(2).offset);
     float intensity =
-        *reinterpret_cast<float *>(addr + msg.fields.at(3).offset);
+        *reinterpret_cast<float *>(addr + msg->fields.at(3).offset);
     uint16_t ring =
-        *reinterpret_cast<uint16_t *>(addr + msg.fields.at(4).offset);
-    float azimuth = *reinterpret_cast<float *>(addr + msg.fields.at(5).offset);
-    float distance = *reinterpret_cast<float *>(addr + msg.fields.at(6).offset);
+        *reinterpret_cast<uint16_t *>(addr + msg->fields.at(4).offset);
+    float azimuth = *reinterpret_cast<float *>(addr + msg->fields.at(5).offset);
+    float distance =
+        *reinterpret_cast<float *>(addr + msg->fields.at(6).offset);
     uint8_t return_type =
-        *reinterpret_cast<uint8_t *>(addr + msg.fields.at(7).offset);
+        *reinterpret_cast<uint8_t *>(addr + msg->fields.at(7).offset);
     double time_stamp =
-        *reinterpret_cast<double *>(addr + msg.fields.at(8).offset);
+        *reinterpret_cast<double *>(addr + msg->fields.at(8).offset);
     pts.push_back(custom_type::Point{x, y, z, intensity, ring, azimuth,
                                      distance, return_type, time_stamp});
   }
@@ -67,17 +88,29 @@ bool deserialize(std::vector<custom_type::Point> &pts,
 }
 
 int main() {
-  /*   printf("PCLは内部的にこのようにFieldを作っている\n");
-    printf("offset x:  %ld\n", offsetof(custom_type::Point, x)); // 0
-    printf("offset y:  %ld\n", offsetof(custom_type::Point, y)); // 4
-    printf("offset z:  %ld\n", offsetof(custom_type::Point, z)); // 8
-    printf("offset intensity:  %ld\n",
-           offsetof(custom_type::Point, intensity));                         //
-    12 printf("offset ring:  %ld\n", offsetof(custom_type::Point, ring)); // 16
-    printf("offset azimuth:  %ld\n", offsetof(custom_type::Point, azimuth)); //
-    20 printf("sizeof(custom_type::Point): %ld\n", sizeof(custom_type::Point));
-    // 24 printf("---\n");
-   */
+
+  printf(" --- ELIGN16付きの構造体からXYZIを作る\n");
+  {
+    pcl::PointCloud<custom_type::PointXYZI>::Ptr pcl_cloud_4d(
+        new pcl::PointCloud<custom_type::PointXYZI>);
+    pcl_cloud_4d->width = 1;
+    pcl_cloud_4d->height = 1;
+    pcl_cloud_4d->points.push_back(custom_type::PointXYZI{1, 2, 3, 4});
+
+    sensor_msgs::msg::PointCloud2::SharedPtr from_pcl_msg_4d(
+        new sensor_msgs::msg::PointCloud2);
+    pcl::toROSMsg(*pcl_cloud_4d, *from_pcl_msg_4d);
+    printf("ELIGN16付きの構造体から作られたField\n");
+    for (int i = 0; i < from_pcl_msg_4d->fields.size(); i++) {
+      printf(" - offset %s: %ld,\n", from_pcl_msg_4d->fields.at(i).name.c_str(),
+             from_pcl_msg_4d->fields.at(i).offset);
+    }
+    printf(" - point_step: %d", from_pcl_msg_4d->point_step);
+  }
+  printf("\n");
+
+  /////////////////////////////////////////////////////
+  printf(" --- ELIGN16付きの構造体からXYZIRADRTを作る\n");
 
   pcl::PointCloud<custom_type::Point>::Ptr pcl_cloud(
       new pcl::PointCloud<custom_type::Point>);
@@ -85,27 +118,31 @@ int main() {
   pcl_cloud->height = 1;
   pcl_cloud->points.push_back(custom_type::Point{1, 2, 3, 4, 5, 6, 7, 8, 9});
 
-  sensor_msgs::msg::PointCloud2 from_pcl_msg;
-  pcl::toROSMsg(*pcl_cloud, from_pcl_msg);
+  sensor_msgs::msg::PointCloud2::SharedPtr from_pcl_msg(
+      new sensor_msgs::msg::PointCloud2);
+  pcl::toROSMsg(*pcl_cloud, *from_pcl_msg);
 
-  printf("toROSMsgによって作られたFieldのoffset\n");
-  for (int i = 0; i < from_pcl_msg.fields.size(); i++) {
-    printf("offset %s: %ld,\n", from_pcl_msg.fields.at(i).name.c_str(),
-           from_pcl_msg.fields.at(i).offset);
+  printf("toROSMsgによって作られたField\n");
+  for (int i = 0; i < from_pcl_msg->fields.size(); i++) {
+    printf(" - offset %s: %ld,\n", from_pcl_msg->fields.at(i).name.c_str(),
+           from_pcl_msg->fields.at(i).offset);
   }
+  printf(" - point_step: %d",
+         from_pcl_msg
+             ->point_step); // ELIGN16の効果で48確保されるサイズ的には40で足りる
   printf("\n");
 
-  printf(" --- serialized data\n");
-  for (int i = 0; i < from_pcl_msg.data.size(); i++) {
-    uint8_t *decimal_ptr = reinterpret_cast<uint8_t *>(&from_pcl_msg.data[i]);
+  printf("生のシリアライズデータ\n");
+  for (int i = 0; i < from_pcl_msg->data.size(); i++) {
+    uint8_t *decimal_ptr = reinterpret_cast<uint8_t *>(&from_pcl_msg->data[i]);
     printf("%d,", *decimal_ptr);
     if ((i + 1) % 4 == 0)
       printf(" | ");
   }
-  printf("\n");
+  printf("\n\n");
   std::vector<custom_type::Point> pts;
   deserialize(pts, from_pcl_msg);
-  std::cout << " --- converted data at idx 0 " << std::endl;
+  std::cout << "キャストしたデータ" << std::endl;
   std::cout << pts[0].x << ", ";
   std::cout << pts[0].y << ", ";
   std::cout << pts[0].z << ", ";
@@ -118,7 +155,6 @@ int main() {
   std::cout << std::endl;
 
   // wrapper
-
   using CustomGenerator =
       std::tuple<point_cloud_msg_wrapper::field_x_generator,
                  point_cloud_msg_wrapper::field_y_generator,
@@ -130,13 +166,57 @@ int main() {
   sensor_msgs::msg::PointCloud2 from_wrapper_msg;
   point_cloud_msg_wrapper::PointCloud2Modifier<custom_type::Point,
                                                CustomGenerator>
-      msg_modifier{from_wrapper_msg, from_pcl_msg.header.frame_id};
-  printf("wrapperによって作られたFieldのoffset\n");
+      msg_modifier{from_wrapper_msg, from_pcl_msg->header.frame_id};
+  printf("wrapperによって作られたField\n");
   for (int i = 0; i < from_wrapper_msg.fields.size(); i++) {
-    printf("offset %s: %ld,\n", from_wrapper_msg.fields.at(i).name.c_str(),
+    printf(" - offset %s: %ld,\n", from_wrapper_msg.fields.at(i).name.c_str(),
            from_wrapper_msg.fields.at(i).offset);
   }
-  printf("\n");
-  msg_modifier.push_back(custom_type::Point{10,11,12,13,14,15,16,17,18});
+  printf(" - point_step: %d", from_wrapper_msg.point_step);
+  printf("\n\n");
 
+
+
+  printf("wrapperのpush_backの性能を試す\n");
+  std::chrono::system_clock::time_point start;
+  start = std::chrono::system_clock::now();
+
+  int vlp128_num = 225628;
+  int num = std::pow(10, 7);
+  std::cerr << "point num: " << num << std::endl;
+
+  msg_modifier.clear();
+  msg_modifier.reserve(num * from_wrapper_msg.point_step);
+
+  for (uint32_t i = 0; i < num; i++) {
+    custom_type::Point pt{10, 11, 12, 13, 14, 15, 16, 17, 18};
+    msg_modifier.push_back(pt);
+  }
+  double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+                       std::chrono::system_clock::now() - start)
+                       .count();
+  std::cerr << "elapsed time (copy): " << elapsed << " (millisec)" << std::endl;
+
+  msg_modifier.clear();
+
+  start = std::chrono::system_clock::now();
+  for (uint32_t i = 0; i < num; i++) {
+    custom_type::Point pt{10, 11, 12, 13, 14, 15, 16, 17, 18};
+    msg_modifier.push_back(std::move(pt));
+  }
+  elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now() - start)
+                .count();
+  std::cerr << "elapsed time (copy): " << elapsed << " (millisec)" << std::endl;
+
+  start = std::chrono::system_clock::now();
+  for (uint32_t i = 0; i < num; i++) {
+    if (i % 2 == 0) {
+      continue;
+    }
+  }
+  elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now() - start)
+                .count();
+  std::cerr << "elapsed time (copy): " << elapsed << " (millisec)" << std::endl;
 }
